cea63da11de99643c3cb804eb296a25f5fd0276a
Julia <julia.zaki@mail.utoronto.ca>
2024-11-11 14:41:56 -0500

    julia v7

--- src/main/java/app/TranslateTextAppBuilder.java
+++ src/main/java/app/TranslateTextAppBuilder.java
@@ -1,84 +1,81 @@
 package app;
 
-import interface_adapter.note.NoteController;
-import interface_adapter.note.NotePresenter;
-import interface_adapter.note.NoteViewModel;
+import interface_adapter.translateText.TranslateTextController;
+import interface_adapter.translateText.TranslateTextPresenter;
 import interface_adapter.translateText.TranslateTextViewModel;
-import use_case.note.NoteDataAccessInterface;
-import use_case.note.NoteInteractor;
-import use_case.note.NoteOutputBoundary;
 import use_case.translateText.TranslateTextDataAccessInterface;
 import use_case.translateText.TranslateTextInteractor;
-import view.NoteView;
+import use_case.translateText.TranslateTextOutputBoundary;
 import view.TranslateTextView;
 
 import javax.swing.*;
 
 /**
- * Builder for the Note Application.
+ * Builder for the Translator Application.
  */
 public class TranslateTextAppBuilder {
     public static final int HEIGHT = 300;
     public static final int WIDTH = 400;
-    private TranslateTextDataAccessInterface translatetextDAO;
-    private TranslateTextViewModel noteViewModel = new TranslateTextViewModel();
-    private TranslateTextView noteView;
-    private TranslateTextInteractor noteInteractor;
+    private TranslateTextDataAccessInterface translateTextDAO;
+    private TranslateTextViewModel translateTextViewModel = new TranslateTextViewModel();
+    private TranslateTextView translateTextView;
+    private TranslateTextInteractor translateTextInteractor;
 
     /**
-     * Sets the NoteDAO to be used in this application.
-     * @param translatetextDataAccess the DAO to use
+     * Sets the translatetextDAO to be used in this application.
+     * @param translateTextDataAccess the DAO to use
      * @return this builder
      */
-    public TranslateTextAppBuilder addtranslatetextDAO(TranslateTextDataAccessInterface translatetextDataAccess) {
-        translatetextDAO = translatetextDataAccess;
+    public TranslateTextAppBuilder addTranslateTextDAO(TranslateTextDataAccessInterface translateTextDataAccess) {
+        translateTextDAO = translateTextDataAccess;
         return this;
     }
 
     /**
-     * Creates the objects for the Note Use Case and connects the NoteView to its
+     * Creates the objects for the TranslateText Use Case and connects the TranslateTextView to its
      * controller.
-     * <p>This method must be called after addNoteView!</p>
+     * <p>This method must be called after addTranslateTextView!</p>
      * @return this builder
-     * @throws RuntimeException if this method is called before addNoteView
+     * @throws RuntimeException if this method is called before addTranslateTextView
      */
-    public TranslateTextAppBuilder addNoteUseCase() {
-        final NoteOutputBoundary noteOutputBoundary = new NotePresenter(noteViewModel);
-        noteInteractor = new NoteInteractor(
-                noteDAO, noteOutputBoundary);
+    public TranslateTextAppBuilder addTranslateTextUseCase() {
+        final TranslateTextOutputBoundary translateTextOutputBoundary =
+                new TranslateTextPresenter(translateTextViewModel);
+        translateTextInteractor = new TranslateTextInteractor(
+                translateTextDAO, translateTextOutputBoundary);
 
-        final NoteController controller = new NoteController(noteInteractor);
-        if (noteView == null) {
-            throw new RuntimeException("addNoteView must be called before addNoteUseCase");
+        final TranslateTextController controller = new TranslateTextController(translateTextInteractor);
+        if (translateTextView == null) {
+            throw new RuntimeException("addTranslateTextView must be called before addTranslateTextUseCase");
         }
-        noteView.setNoteController(controller);
+        translateTextView.setTranslateTextController(controller);
         return this;
     }
 
     /**
-     * Creates the NoteView and underlying NoteViewModel.
+     * Creates the TranslateTextView and underlying TranslateTextViewModel.
      * @return this builder
      */
-    public TranslateTextAppBuilder addNoteView() {
-        noteViewModel = new NoteViewModel();
-        noteView = new NoteView(noteViewModel);
+    public TranslateTextAppBuilder addTranslateTextView() {
+        translateTextViewModel = new TranslateTextViewModel();
+        translateTextView = new TranslateTextView(translateTextViewModel);
         return this;
     }
 
     /**
-     * Builds the application.
+     * Builds the translator application.
      * @return the JFrame for the application
      */
     public JFrame build() {
         final JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setTitle("Note Application");
+        frame.setTitle("Translator Application");
         frame.setSize(WIDTH, HEIGHT);
 
-        frame.add(noteView);
+        frame.add(translateTextView);
 
-        // refresh so that the note will be visible when we start the program
-        noteInteractor.executeRefresh();
+        // refresh so that the TranslateText will be visible when we start the program
+        translateTextInteractor.executeRefresh();
 
         return frame;
 
